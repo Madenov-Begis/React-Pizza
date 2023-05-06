f973be3e8727dda27357b54d65836221925e6ad4
Madenov-Begis <begismadenov@gmail.com>
2023-05-07 01:24:06 +0300

    ReactPizza2

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import classNames from 'classnames'
+import React, { Children } from 'react'
+
+function Button({ className, children }) {
+  return (
+    <button className={classNames('button', className)}>
+      {children}
+    </button>
+  )
+}
+
+export default Button
---
+++ src/components/Categories.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+function Categories({ items }) {
+  const [activeItem, setActiveItem] = useState(null)
+
+  const onSelectItem = (index) => {
+    setActiveItem(index)
+  }
+  return (
+    <div className="categories">
+      <ul>
+        <li
+          onClick={() => onSelectItem(null)}
+          className={activeItem === null ? 'active' : ''}
+        >
+          Все
+        </li>
+        {items.map((item, index) => {
+          return (
+            <li
+              onClick={() => onSelectItem(index)}
+              className={activeItem === index ? 'active' : ''}
+              key={`${item}_${index}`}
+            >
+              {item}
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default Categories
